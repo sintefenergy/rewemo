3831af2789fe7a2b8c2d8a492764afedf47fdda1
Harald G Svendsen <harald.svendsen@sintef.no>
2022-03-04 14:25:08 +0100

    refactor

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-from setuptools import setup
-
-setup(name='rewemo',
-      version='0.1.0',
-      description='Renewable energy time series from numerical weather model data',
-	  long_description = '',
-      url='',
-      author='Harald G Svendsen',
-      author_email='harald.svendsen@sintef.no',
-      license='MIT License (http://opensource.org/licenses/MIT)',
-      packages=['rewemo'],
-      zip_safe = True,
-	  classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 3',
-	  ],
-	  keywords = 'renewable energy',
-	 )
+from distutils.core import setup
+setup(
+	name = "rewemo",
+	version = "0.1.0",
+	description = "Renewable energy time series from numerical weather model data",
+	author = "Harald G Svendsen",
+	author_email = "harald.svendsen@sintef.no",
+	license = "MIT License (http://opensource.org/licenses/MIT)",
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License"
+		],
+#	packages = ["rewemo"],
+	package_dir= {"": "src/rewemo"},
+	python_requires = ">=3.7"
+)
