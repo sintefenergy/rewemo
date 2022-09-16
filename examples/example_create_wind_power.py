a2d2d030abf46f2efbb10f58c40733064391383d
aarvesen <anders.arvesen@sintef.no>
2022-09-16 13:16:53 +0200

    Small adjustments example_create_wind_power file

--- examples/example_create_wind_power.py
+++ examples/example_create_wind_power.py
@@ -10,7 +10,7 @@ from helper_functions import (calculate_indicators_windp)
 file_wpp_locations = "wpp_locations.yaml"
 file_power_curves = "../src/rewemo/ncep_reanalysis/wind_powercurves_tradewind.csv"
 files_wind_data = "../data/data_europe_era5/era5data_month=2021-11.grib"
-output_path = Path("./output_wind_timeseries")
+output_path = Path("/output_wind_timeseries")
 
 with open(file_wpp_locations, "r", encoding="utf8") as f:
     wpp_locations = pd.DataFrame.from_dict(safe_load(f), orient="index")
@@ -20,14 +20,16 @@ logging.info("Extracting wind data from ERA5 GRIB files (may take some time)")
 wind_data = era5.extract_wind_speed_at_locations(files_wind_data, wpp_locations)
 
 # Initiate dataframe to hold diagnostic indicator values
-key_indicators = pd.DataFrame(index=wind_data.keys(), columns = [
+KEY_INDICATORS_NAMES = [
     'capacity factor (%)', 'full load hours (h)',
     'average power', 'median power', 
     'time fraction: zero power (%)',
     'time fraction: zero power low wind (%)',
     'time fraction: zero power high wind (%)',
     'time fraction: max power (%)',
-    'min power', 'min power excl. zero power', 'max power', 'stdev power'])
+    'min power', 'min power excl. zero power', 'max power', 'stdev power']
+key_indicators = pd.DataFrame(index=wind_data.keys(),
+    columns=KEY_INDICATORS_NAMES)
 
 # Create output folder if it doesn't already exist:
 Path(output_path).mkdir(parents=True, exist_ok=True)
