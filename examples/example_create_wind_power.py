2c2d6e9d35b911f6e4389cb64974ae575e8999ee
aarvesen <anders.arvesen@sintef.no>
2023-03-07 08:27:52 +0100

    Add loop for computing time series multiple areas

--- examples/example_create_wind_power.py
+++ examples/example_create_wind_power.py
@@ -8,64 +8,74 @@ from helper_functions import (
     calculate_indicators_windp, compute_wind_power2)
 
 # Modify these:
-file_wpp_locations = "wpp_locations_NORGEMIDT.yaml"
+areas = ['VESTSYD', 'NORGEMIDT'] # Larger (aggregate) areas considered
 file_power_curves = "../src/rewemo/ncep_reanalysis/wind_powercurves_tradewind.csv"
 files_wind_data = "../data/data_europe_era5/era5data_month=20*.grib"
 output_path = "output_wind_timeseries/"
 
-with open(file_wpp_locations, "r", encoding="utf8") as f:
-    wpp_locations = pd.DataFrame.from_dict(safe_load(f), orient="index")
 power_curves = pd.read_csv(file_power_curves, index_col=0)
-
-logging.info("Extracting wind data from ERA5 GRIB files (may take some time)")
-wind_data = era5.extract_wind_speed_at_locations(files_wind_data, wpp_locations)
-
-# Initiate dataframe to hold diagnostic indicator values
-KEY_INDICATORS_NAMES = [
-    'capacity factor (%)', 'full load hours (h)',
-    'average power', 'median power', 
-    'time fraction: zero power (%)',
-    'time fraction: zero power low wind (%)',
-    'time fraction: zero power high wind (%)',
-    'time fraction: max power (%)',
-    'min power', 'min power excl. zero power', 'max power', 'stdev power',
-    'alpha', 'beta']
-key_indicators = pd.DataFrame(index=wind_data.keys(),
-    columns=KEY_INDICATORS_NAMES)
 
 # Create output folder if it doesn't already exist:
 Path(output_path).mkdir(parents=True, exist_ok=True)
 
-for i, wpp in wpp_locations.iterrows():
-    logging.info(f"Location {i}")
-    pcurve_ref = wpp["power_curve"]
-    pcurve = power_curves[pcurve_ref]
+file_wpp_locations = []
+for area_index, area in enumerate(areas):
+    file_wpp_locations.append("wpp_locations_"+area+".yaml")
     
-    # If capacity factor target not defined in input data, add assumption here
-    if "cf_target" not in wpp.index:
-        if wpp["power_curve"] == "offshore":
-            wpp["cf_target"] = 0.40
-        else:
-            wpp["cf_target"] = 0.35
+    with open(file_wpp_locations[area_index], "r", encoding="utf8") as f:
+        wpp_locations = pd.DataFrame.from_dict(safe_load(f), orient="index")
     
-    # Use either option 1 or option #2 below
-    # Option 1: predefined scaling factor for wind speed
-    # wind_scaling = wpp["wind_scaling"]
-    # windp = wp.compute_wind_power(df_wind=wind_data[i], df_power_curve=pcurve, wind_scaling=wind_scaling)
-    # Option 2: scale to predefined average capacity factor
-    (windp, alpha, beta) = compute_wind_power2(
-        df_wind=wind_data[i], df_power_curve=pcurve, cf_target=wpp["cf_target"])
-    key_indicators['alpha'].loc[i] = alpha
-    key_indicators['beta'].loc[i] = beta
+    logging.info("Extracting wind data from GRIB files (may take some time)")
+    wind_data = era5.extract_wind_speed_at_locations(
+        files_wind_data, wpp_locations)
     
-    wind_data[i]["power"] = windp
+    # Initiate dataframe to hold diagnostic indicator values
+    KEY_INDICATORS_NAMES = [
+        'capacity factor (%)', 'full load hours (h)',
+        'average power', 'median power', 
+        'time fraction: zero power (%)',
+        'time fraction: zero power low wind (%)',
+        'time fraction: zero power high wind (%)',
+        'time fraction: max power (%)',
+        'min power', 'min power excl. zero power', 'max power', 'stdev power',
+        'alpha', 'beta']
+    key_indicators = pd.DataFrame(index=wind_data.keys(),
+        columns=KEY_INDICATORS_NAMES)
     
-    # Calculate diagnostic indicator values
-    key_indicators = calculate_indicators_windp(
-        wind_data, pcurve, key_indicators, i)
+    for i, wpp in wpp_locations.iterrows():
+        logging.info(f"Location {i}")
+        pcurve_ref = wpp["power_curve"]
+        pcurve = power_curves[pcurve_ref]
+        
+        # If capacity factor target not defined in input data, add assumption
+        if "cf_target" not in wpp.index:
+            if wpp["power_curve"] == "offshore":
+                wpp["cf_target"] = 0.40
+            else:
+                wpp["cf_target"] = 0.35
+        
+        # Use either option 1 or option #2 below
+        # Option 1: predefined scaling factor for wind speed
+        # wind_scaling = wpp["wind_scaling"]
+        # windp = wp.compute_wind_power(df_wind=wind_data[i],
+        #     df_power_curve=pcurve, wind_scaling=wind_scaling)
+        # Option 2: scale to predefined average capacity factor
+        (windp, alpha, beta) = compute_wind_power2(
+            df_wind=wind_data[i], df_power_curve=pcurve,
+            cf_target=wpp["cf_target"])
+        key_indicators['alpha'].loc[i] = alpha
+        key_indicators['beta'].loc[i] = beta
+        
+        wind_data[i]["power"] = windp
+        
+        # Calculate diagnostic indicator values
+        key_indicators = calculate_indicators_windp(
+            wind_data, pcurve, key_indicators, i)
+        
+        # Save to file
+        csv_filename = output_path +"windpower_"+str(i)+".csv"
+        wind_data[i].to_csv(csv_filename, sep=';')
     
     # Save to file
-    csv_filename = output_path +"windpower_"+str(i)+".csv"
-    wind_data[i].to_csv(csv_filename, sep=';')
-    csv_filename = output_path +"key_indicators_"+str(i)+".csv"
+    csv_filename = output_path +"key_indicators_"+str(area)+".csv"
     key_indicators.to_csv(csv_filename, sep=';')
